3c726afe97b4bf52866a67394ce7fbea21d66e00
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-08 15:06:58 -0600

    commit tree 19d6624f98fb3238229f4288b8ae272d61b1c1c7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    //return matching list of drivers, case insensitive 
+    let match = drivers.filter(driver => {
+        return driver === string || driver === string.toLowerCase()
+    })
+    return match
+}
+
+function fuzzyMatch(drivers, string) {
+    //returns all drivers whose name begins with provided letters
+    return drivers.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+}
+
+function matchName(drivers, string){
+    // return each element whose name prop. matches the provided string arg
+    return drivers.filter(function(object) { 
+        if (object["name"] === string){
+        return object 
+        }
+    })
+}
